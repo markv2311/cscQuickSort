87af94cb6be8df0eb4e13c6f4682067ac11b93a1
mmorano16 <msm03648@lincoln.sjfc.edu>
2019-10-31 13:15:59 -0400

    finished the quicksort method

--- Quick.java
+++ Quick.java
@@ -2,8 +2,13 @@ public class Quick {
     public static void main (String args[]){
         int[] arry = {7,2,1,5,3,10,4,6};
     }
-    public static void quickSort(int [] a, int left, int right){
-
+    public static void quickSort(int arr[], int left, int right){
+	if(left>=right)
+		return;
+	int pivot = arr(left+right)/2;
+	int index = partition(arr, left, right, pivot);
+	quicksort(arr, left, index - 1);
+	quicksort(arr, index, right);
     }
     public static void partition(int[] a, int left, int right, int pivot){
         
