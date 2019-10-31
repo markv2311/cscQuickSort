0489ae0ab5fd4e43055449ca1e931842a8daa816
Mark Valeriani <markv2311@gmail.com>
2019-10-31 12:47:36 -0400

    First commit for quickSort

---
+++ Quick.java
@@ -0,0 +1,11 @@
+public class Quick {
+    public static void main (String args[]){
+        int[] arry = {7,2,1,5,3,10,4,6};
+    }
+    public static void quickSort(int [] a, int left, int right){
+
+    }
+    public static void partition(int[] a, int left, int right, int pivot){
+        
+    }
+}
